13bdde2f27efcfe54290bd6301e7549ef28ee181
Nikhil Mishra <nikhilmishra4735@gmail.com>
2026-05-10 19:51:18 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all js codes as newer version
+
+// alert("3 + 3 = 6   "); // were using node js not browser 
+
+console.log("3 + 3 );
+
+console.log("Nikhil")
---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountID = 144553
+let accountEmail = "user@example.com"
+var accountPassword = "password123"
+accountCity = "New York"
+let accountState;
+
+// accountID = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "newpassword456"
+accountCity = "Los Angeles"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
